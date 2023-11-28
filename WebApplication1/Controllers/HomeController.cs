2605419fb40c8ed6b1c8323e5efd74be08a176b3
paul <22400039@usc.edu.ph>
2023-11-28 19:58:11 +0800

    28

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Layout()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult AddUserToDatabase(FormCollection fc)
+        {
+            String firstName = fc["firstname"];
+            String lastName = fc["lastname"];
+            String email = fc["email"];
+            String diko = fc["password"];
+
+            user use = new user();
+            use.firstname = firstName;
+            use.lastname = lastName;
+            //use.Email = email;
+            //use.Password = diko;
+            use.Role_id = 1;
+
+
+
+            UsersEntities fe = new UsersEntities();
+            fe.users.Add(use);
+            fe.SaveChanges();
+
+            //insert the code that will save these information to the DB
+
+            return RedirectToAction("Input");
+        }
+    }
+}
